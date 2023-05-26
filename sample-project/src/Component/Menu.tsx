a83a4a6f717ea86965e8c33e314eca6c3a61e869
Rachit Desai <Rachit.Desai@RACHITDES-LT>
2023-05-26 11:33:38 +0530

    update 1

--- sample-project/src/Component/Menu.tsx
+++ sample-project/src/Component/Menu.tsx
@@ -1,11 +1,17 @@
-function Menu()
-{
-    return <><ul>Rachit</ul>
-             <ul>Desai</ul>
-            <ul>Banglore</ul>    
-    
+import { MouseEvent } from "react";
+function Menu() {
+  const items = ["Home", "About Us", "Contact Us ", "Sign In"];
+  const handleClick = (event: MouseEvent) => console.log(event);
+  return (
+    <>
+      <ul className="list-group">
+        {items.map((x) => (
+          <li className="list-group-item" key={x} onClick={handleClick}>
+            {x}
+          </li>
+        ))}
+      </ul>
     </>
+  );
 }
-
-
-export default Menu
+export default Menu;
